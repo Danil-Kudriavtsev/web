4f8b24d3a6c9b836398cf200c954dc61a2b1b500
Danil-Kudriavtsev <159793154+Danil-Kudriavtsev@users.noreply.github.com>
2024-06-10 21:17:04 +0300

    Add files via upload

---
+++ lw_5/post-preview-first.php
@@ -0,0 +1,13 @@
+<div class="<?= $post['class'] ?>">
+    <a class="featured-post__card_link" href='/post?id=<?=$post['id'] ?>'></a>
+    <div class="featured-post__content">
+        <div class="featured-post__picture"><?= $post['main_img'] ?></div>
+        <div class="featured-post__content_title"><?= $post['title'] ?></div>
+        <div class="featured-post__content_subtitle"><?= $post['subtitle'] ?></div>
+        <div class="featured-post__content_footer">
+            <span class="featured-post__footer_img"><?= $post['img_modifier'] ?></span>
+            <span class="featured-post__footer_author"><?= $post['author'] ?></span>
+            <span class="featured-post__footer_data"><?= $post['data'] ?></span>
+        </div>
+    </div>
+</div>
